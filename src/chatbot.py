1ea3113549160a86619d1efb98535b5172d11850
Marterminator <martin.quanz@gmail.com>
2018-03-24 15:24:56 +0100

    updated keywords and answers

--- src/chatbot.py
+++ src/chatbot.py
@@ -13,6 +13,21 @@ def handle(msg):
         if content_type == 'text' and any(keyword in msg['text'].lower() for keyword in keywords):
             bot.sendMessage(chat_id, random.choice(DATA[keywords]))
 
+    if content_type == 'text' and msg['text'].lower() == 'selbstzerstörung':
+        bot.sendMessage(chat_id, '10')
+        bot.sendMessage(chat_id, '9')
+        bot.sendMessage(chat_id, '8')
+        bot.sendMessage(chat_id, '7')
+        bot.sendMessage(chat_id, '6')
+        bot.sendMessage(chat_id, '5')
+        bot.sendMessage(chat_id, '4')
+        bot.sendMessage(chat_id, '3')
+        bot.sendMessage(chat_id, '2')
+        bot.sendMessage(chat_id, '1')
+        bot.sendMessage(chat_id, '0')
+        bot.sendMessage(chat_id, '...')
+        bot.sendMessage(chat_id, 'Ups.. Das hat wohl leider nicht geklappt.')
+        
     if content_type == 'photo' or content_type == 'video':
         bot.sendMessage(chat_id, 'Das ist doch ein Porno!')
 
