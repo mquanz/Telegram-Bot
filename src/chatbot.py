f7684775a50c32fcb25b5338cb8319dd1f26bffb
Marterminator <martin.quanz@gmail.com>
2018-03-24 15:02:14 +0100

    reduced if conditions by looping over dict

--- src/chatbot.py
+++ src/chatbot.py
@@ -3,32 +3,18 @@ import telepot
 from telepot.loop import MessageLoop
 import random
 from keywords import *
-
+        
 def handle(msg):
     # Read information of user writing.
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
+    
+    for keywords in DATA:
+        if content_type == 'text' and any(keyword in msg['text'].lower() for keyword in keywords):
+            bot.sendMessage(chat_id, random.choice(DATA[keywords]))
 
-    if content_type == 'text' and any(keyword in msg['text'].lower() for keyword in SAUFEN):
-        bot.sendMessage(chat_id, random.choice(ANSW_SAUFEN))
-
-    elif content_type == 'text' and any(keyword in msg['text'].lower() for keyword in BIER):
-        bot.sendMessage(chat_id, random.choice(ANSW_BIER))
-
-    elif content_type == 'sticker':
-        bot.sendMessage(chat_id, random.choice(SMILEYS))
-
-    elif content_type == 'text' and any(keyword in msg['text'].lower() for keyword in VEGAN):
-        bot.sendMessage(chat_id, ANSW_VEGAN)
-
-    elif content_type == 'photo' or content_type == 'video':
+    if content_type == 'photo' or content_type == 'video':
         bot.sendMessage(chat_id, 'Das ist doch ein Porno!')
-
-    elif content_type == 'text' and any(keyword in msg['text'].lower() for keyword in NAME):
-        bot.sendMessage(chat_id, random.choice(ANSW_NAME))
-
-    elif content_type == 'text' and any(keyword in msg['text'].lower() for keyword in WASGEHT):
-        bot.sendMessage(chat_id, ANSW_WASGEHT)
 
 # To get your own token, contact @BotMaster in Telegram.
 with open('token.txt') as f:
--- src/keywords.py
+++ src/keywords.py
@@ -1,14 +1,1 @@
-# Keywords:
-SAUFEN = ['saufen', 'ballern', 'trinken', 'alkohol']
-BIER = ['bier']
-VEGAN = ['vegetari', 'vegan']
-NAME = ['richard']
-WASGEHT = ['was geht']
-
-# Answers:
-ANSW_SAUFEN = ['JAMAN, richtig was installieren!', 'Ich will BALLERN!', 'Morgens, mittags, abends ich will saufen!']
-ANSW_BIER = ['Hab ich da eben Bier gehört?', 'Ja geil, schön rein in den Schlund!', 'Bier ist eben ein Grundnahrungsmittel.']
-SMILEYS = ['Hübscher Smiley..', 'Ich hasse diese Sticker!', 'Hört doch endlich mal auf mit diesen Stickern!!!11!!']
-ANSW_VEGAN = 'Hab ich da gerade Hack gehört?'
-ANSW_NAME= ['Ist doch egal, lass mal was installieren!', 'Weiß ich auch nicht', 'Döp Döp Döp DöDöDöp Döp Döp DöDöp Döp Döp DöDöDöp Döp Döp']
-ANSW_WASGEHT = 'Alles was Beine hat.'
+DATA = {('saufen', 'ballern', 'trinken', 'alkohol'):('JAMAN, richtig was installieren!', 'Ich will BALLERN!', 'Morgens, mittags, abends ich will saufen!'), ('bier', 'soße'):('Hab ich da eben Bier gehört?', 'Ja geil, schön rein in den Schlund!', 'Bier ist eben ein Grundnahrungsmittel.'), ('vegetari', 'vegan'):('Hab ich da gerade Hack gehört?', 'Hmmm, Veganer schmecken mir am besten ...'), ('richard', 'ritschie'):('Ist doch egal, lass mal was installieren!', 'Weiß ich auch nicht', 'Döp Döp Döp DöDöDöp Döp Döp DöDöp Döp Döp DöDöDöp Döp Döp'), ('was geht', 'whatup'):('Alles was Beine hat.', 'Nichts')}
